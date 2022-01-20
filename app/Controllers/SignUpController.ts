0791b680658ed52166d7da4340d2325f1457f503
Richard Alejo <richardalejo@Richards-MacBook-Air.local>
2022-01-20 19:08:54 +0800

    Doc-969-Create-Seeder-for-the-categories

--- app/Controllers/SignUpController.ts
+++ app/Controllers/SignUpController.ts
@@ -14,8 +14,6 @@ export default class SignUpController {
 			"first_name",
 			"middle_name",
 			"last_name",
-			"birth_date",
-			"gender"
 		])
 
 		const user = await UserRepository.query()
@@ -30,8 +28,6 @@ export default class SignUpController {
 		clients.first_name = data.first_name,
 			clients.middle_name = data.middle_name,
 			clients.last_name = data.last_name,
-			clients.birth_date = data.birth_date,
-			clients.gender = data.gender,
 			await clients.save()
 
 		const users = new UserRepository()
