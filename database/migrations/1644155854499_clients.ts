f9af090957e1cebb7b3899284e8b1447dfc8c4f5
Richard Alejo <richardalejo@Richards-MacBook-Air.local>
2022-02-07 08:00:53 +0800

    update

--- database/migrations/1644155854499_clients.ts
+++ database/migrations/1644155854499_clients.ts
@@ -5,6 +5,7 @@ export default class Clients extends BaseSchema {
 
   public async up() {
     this.schema.alterTable(this.tableName, (table) => {
+      table.string('last_name',75).after('middle_name')
       table.string('contact',15).after('last_name')
      
     })
@@ -12,6 +13,7 @@ export default class Clients extends BaseSchema {
 
   public async down() {
     this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('last_name')
       table.dropColumn('contact')
     })
   }
