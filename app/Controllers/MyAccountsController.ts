2d486cfb9f620cd8a61ebaff56d93b09048d7aa4
meljie13 <mjestillana13@gmail.com>
2022-03-14 10:55:34 +0800

    add user id in my account endpoint

--- app/Controllers/MyAccountsController.ts
+++ app/Controllers/MyAccountsController.ts
@@ -1,11 +1,17 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Response from 'App/Helpers/Response'
 import ClientRepository from 'App/Repositories/ClientRepository'
 import MyAccountTransformer from 'App/Transformers/MyAccountTransformer'
 
 export default class MyAccountsController {
-    async index({ auth, response, transform }) {
-        const user = auth.user
-        const client = await ClientRepository.query().where('id', user.profile_id)
+    async index({request, response, transform }) {
+        const userId = request.input('userId')
+        console.log("UserId: " + userId)
+        const apiResponse = new Response(response)
+        const client = await ClientRepository.query().where('id', userId).first()
+        if(!client) {
+          return apiResponse.notFound('Not found id: ' + userId)
+        }
         return response.resource(await transform.item(client, MyAccountTransformer))
       }
     
