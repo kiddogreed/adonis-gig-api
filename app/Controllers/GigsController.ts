c9065cce1f38a5e3358859281b1c0705833838fc
Richard Alejo <richardalejo@Richards-MacBook-Air.local>
2022-03-15 14:15:26 +0800

    update

--- app/Controllers/GigsController.ts
+++ app/Controllers/GigsController.ts
@@ -59,7 +59,7 @@ export default class GigsController {
     await request.validate(GigValidator)
     const user = auth.user
     const data = request.input([`tag`])
-    try {
+    // try {
 
       const gig = await GigRepository.create({
         client_id: user.profile_id,
@@ -97,9 +97,9 @@ export default class GigsController {
 
       return response.data({ 'id': gig.id }, 'Gig information successfully created')
 
-    } catch (e) {
-      return response.badRequest('Invalid Gig Request')
-    }
+    // } catch (e) {
+    //   return response.badRequest('Invalid Gig Request')
+    // }
   }
 
   async update({ request, params, response }) {
