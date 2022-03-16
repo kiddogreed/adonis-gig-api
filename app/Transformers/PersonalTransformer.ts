d02c40c1e704fbe1d38431373d29b8461e3a311f
meljie13 <mjestillana13@gmail.com>
2022-03-16 11:50:19 +0800

    add my account in profile

--- app/Transformers/PersonalTransformer.ts
+++ app/Transformers/PersonalTransformer.ts
@@ -2,10 +2,19 @@ import ClientRepository from 'App/Repositories/ClientRepository'
 import { TransformerAbstract } from '@ioc:Adonis/Addons/Bumblebee'
 import LanguageRepository from 'App/Repositories/LanguageRepository'
 import UserRepository from 'App/Repositories/UserRepository'
+import LinkAccountRepository from 'App/Repositories/LinkAccountRepository'
+import SkillRepository from 'App/Repositories/SkillRepository'
+import EducationRepository from 'App/Repositories/EducationRepository'
+import CertificationRepository from 'App/Repositories/CertificationRepository'
 export default class PersonalTransformer extends TransformerAbstract {
   public async transform(info: ClientRepository) {
+
     const language = await LanguageRepository.query().where('client_id', info.id)
     const user = await UserRepository.findBy('profile_id', info.id)
+    const linkAccounts = await LinkAccountRepository.query().where('client_id', client.id)
+    const skills = await SkillRepository.query().where('client_id', client.id)
+    const educations = await EducationRepository.query().where('client_id', client.id)
+    const certifications = await CertificationRepository.query().where('client_id', client.id)
 
     return {
       id: info.id,
@@ -21,7 +30,11 @@ export default class PersonalTransformer extends TransformerAbstract {
       photo: info.photo,
       description: info.description,
       website: info.personal_website,
-      language: language
+      language: language,
+      linked_accounts: linkAccounts,
+      skills: skills,
+      educations: educations,
+      certifications: certifications
     }
   }
 }
