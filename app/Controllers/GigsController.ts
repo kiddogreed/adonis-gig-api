9b5f3079ae9491c5a7ae06d682b3521cb56b4af1
Richard Alejo <richardalejo@Richards-MacBook-Air.local>
2022-03-16 08:43:15 +0800

    update

--- app/Controllers/GigsController.ts
+++ app/Controllers/GigsController.ts
@@ -60,7 +60,7 @@ export default class GigsController {
     const user = auth.user
     const data = request.input([`tag`])
 
-    try {
+    // try {
       const gig = await GigRepository.create({
         client_id: user.profile_id,
         name: request.input('title'),
@@ -72,31 +72,48 @@ export default class GigsController {
 
       for (let tag of data) {
         let existingTag = await TagRepository.findBy('name', tag.tag)
-
-        if (existingTag == undefined) {
+      
+        if (!existingTag) {
           const tags = await TagRepository.create({
             name: tag.tag
           })
           await tags.save()
-          await GigTagRepository.firstOrCreate({
-            gig_id: gig.id,
-            tag_id: tags.id
-          })
         }
-        if (existingTag) {
-          await GigTagRepository.firstOrCreate({
-            gig_id: gig.id,
-            tag_id: existingTag.id
-          })
-        }
-        await gig.save()
+      
+       const gigs = await GigTagRepository.firstOrCreate({
+          gig_id: gig.id,
+          tag_id: existingTag?.id
+        })
+        await gigs.save()
       }
+
+      // for (let tag of data) {
+      //   let existingTag = await TagRepository.findBy('name', tag.tag)
+
+      //   if (existingTag == undefined) {
+      //     const tags = await TagRepository.create({
+      //       name: tag.tag
+      //     })
+      //     await tags.save()
+      //     await GigTagRepository.firstOrCreate({
+      //       gig_id: gig.id,
+      //       tag_id: tags.id
+      //     })
+      //   }
+      //   if (existingTag) {
+      //     await GigTagRepository.firstOrCreate({
+      //       gig_id: gig.id,
+      //       tag_id: existingTag.id
+      //     })
+      //   }
+      //   await gig.save()
+      // }
       return response.data({ 'id': gig.id }, 'Gig information successfully created')
 
-    } catch (e) {
-      console.log(e)
-      return response.badRequest('Invalid Gig Request')
-    }
+    // } catch (e) {
+    //   console.log(e)
+    //   return response.badRequest('Invalid Gig Request')
+    // }
   }
 
   async update({ request, params, response }) {
