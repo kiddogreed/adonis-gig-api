58a72139f7f9ace12427d1715f08ccdc6eb01065
john russelle <kiddogreed@gmail.com>
2022-03-18 16:21:07 +0800

    update data

--- app/Controllers/GigSearchesController.ts
+++ app/Controllers/GigSearchesController.ts
@@ -12,71 +12,6 @@ import GigListTransformer from 'App/Transformers/GigListTransformer'
 
 export default class GigSearchesController {
 
-  public async index({response, request, transform}:HttpContextContract){
-    
-    // const gigs = GigRepository.all()
-
- 
-    const page = request.input('page', 1)
-    const limit = 10
-
-    const gigs = await Database.from('gigs').paginate(page, limit)
-
-    
-  //  // console.log(gigs)
-   // const gigs = await GigRepository
-    // .query()
-    // .preload('tags')
-    //console.log(gigs);
-     
-    //  let data = {
-
-    //   id:gigs.
-    //   // client_id: tag.$original.client_id,
-    //   // title: tag.name,
-    //   // tag: tag.$original.tag,
-    //   // category_id: tag.$original.category_id,
-    //   // category: category?.name,
-    //   // subcategory_id: tag.$original.subcategory_id,
-    //   // subcategory: subcategory?.name,
-    //   // tags: tags
-
-    // }
-    
-    //     return response.data(gigs)
-    return response.resource(await transform.collection(gigs, GigTransformer))
-      
-    
-  }
-
-  public async show({ response, params:{gig_id}, transform}:HttpContextContract){
-   // const user = auth.user
-   const gigs = await GigRepository.findByOrFail('id', gig_id)
-   return gigs
-    // const gigs = await GigRepository.findBy('id',params.id)
-    // console.log( gigs);
-    // return gigs
-    // try {
-    //   const gigs = await GigRepository.findBy('id',params.id)
-    //   console.log( gigs);
-      
-    //   if(!gigs){
-    //     return response.badRequest('Gig not found!')
-    //    }
-    //    return response.resource(await transform.item(gigs, GigTransformer))
-    // } catch (error) {
-    //   return response.badRequest('Invalid Request')
-    // }
-    // // const gig = await GigRepository.findByOrFail('id', params.gig_id)
-    // // console.log(gig);
-    // // console.log(gig_id);
-    
-    // console.log(params);
-    
-    
-
-    }
-
   public async search({response, request, transform}:HttpContextContract){
 
   
@@ -139,9 +74,11 @@ return data
   public async find({response, request, transform}:HttpContextContract){
 
     const meta = request.only(["page", "per_page"]);
-    const filters = request.only("search");
-    const gigs = await GigRepository.query().preload('tags')
-      .where('name', 'like', `%${filters.search}%`)
+    const filters = request.only(["by_name", "by_tag"]);
+
+    if (filters.by_name) {
+      const gigs = await GigRepository.query().preload('tags')
+      .where('name', 'like', `%${filters.by_name}%`)
       .orderBy("id", "desc")
       .paginate(meta.page, meta.per_page)
       //  gig.related('tags').query() // 
@@ -149,6 +86,18 @@ return data
 
       return response.resource(await transform.collection(gigs, GigListTransformer))
       
+    }
+    //search by tag query
+ if(filters.by_tag){
+  const tags = await TagRepository.query()
+  .where('name', 'like', `%${filters.by_tag}%`)
+  .orderBy("id", "desc")
+  .paginate(meta.page, meta.per_page)
+
+ return tags
+  //return response.resource(await transform.collection(tags, GigListTransformer))
+ }
+
    
   }
 
